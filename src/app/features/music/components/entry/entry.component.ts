21f830ac0e37c97a9218b3e7fd7a37e803450048
Lindsay Parker <thekillustrator@gmail.com>
2020-06-10 15:37:13 +0000

    forms!

--- src/app/features/music/components/entry/entry.component.ts
+++ src/app/features/music/components/entry/entry.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-entry',
@@ -7,9 +8,28 @@ import { Component, OnInit } from '@angular/core';
 })
 export class EntryComponent implements OnInit {
 
-  constructor() { }
+  songForm: FormGroup;
+  constructor(private formBuilder: FormBuilder) { }
 
   ngOnInit(): void {
+    this.songForm = this.formBuilder.group({
+      title: ['', [Validators.required, Validators.maxLength(50)]],
+      artist: ['', [Validators.required, Validators.maxLength(50)]],
+      album: ['', [Validators.required, Validators.maxLength(50)]]
+    });
   }
 
+  get title() {
+    return this.songForm.get('title');
+  }
+  get artist() {
+    return this.songForm.get('artist');
+  }
+  get album() {
+    return this.songForm.get('album');
+  }
+
+  submit() {
+    console.log(this.songForm.value);
+  }
 }
