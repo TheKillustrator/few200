3a453545cc40f61aeb117c81642636494a027561
Lindsay Parker <thekillustrator@gmail.com>
2020-06-10 18:16:47 +0000

    sort reducer and selector

--- src/app/features/music/reducers/index.ts
+++ src/app/features/music/reducers/index.ts
@@ -1,15 +1,18 @@
 import { ActionReducerMap, createFeatureSelector, createSelector } from '@ngrx/store';
 import * as fromSongs from './songs.reducer';
+import * as fromSongsUiHints from './songs-ui-hints.reducer';
 import { Song } from '../models';
 
 export const featureName = 'music';
 
 export interface MusicState {
   songs: fromSongs.SongState;
+  songsUiHints: fromSongsUiHints.SongsUiHints;
 }
 
 export const reducers: ActionReducerMap<MusicState> = {
-  songs: fromSongs.reducer
+  songs: fromSongs.reducer,
+  songsUiHints: fromSongsUiHints.reducer
 };
 
 //  1. Feature Selector
@@ -21,11 +24,38 @@ const selectSongBranch = createSelector(
   f => f.songs
 );
 
+const selectSongUiHintsBranch = createSelector(
+  selectMusicFeature,
+  f => f.songsUiHints
+);
+
 // 3. "Helpers"
 const { selectAll: selectSongEntityArray } = fromSongs.adapter.getSelectors(selectSongBranch);
 
 // 4. What the components need
-export const selectSongs = createSelector(
+const selectSongsUnsorted = createSelector(
   selectSongEntityArray, // SongEntity[]
   (songs) => songs as Song[] // Song[] - what the component needs
+);
+
+export const selectSortingSongsBy = createSelector(
+  selectSongUiHintsBranch,
+  b => b.sortingBy
+);
+
+export const selectSongs = createSelector(
+  selectSongsUnsorted,
+  selectSortingSongsBy,
+  (songs, by) => {
+    return [...songs.sort((lhs, rhs) => {
+      // bracket notation only works because we chose strings that match field names
+      if (lhs[by].toLowerCase() < rhs[by].toLowerCase()) {
+        return -1;
+      }
+      if (lhs[by].toLowerCase() > rhs[by].toLowerCase()) {
+        return 1;
+      }
+      return 0;
+    })];
+  }
 );
